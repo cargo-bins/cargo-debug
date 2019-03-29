83c5d24f23be6983bb71930b6b6d3e06f147e4f9
Ryan Kurte <ryankurte@gmail.com>
2019-03-29 16:24:15 +1300

    added lldb support, fixed some bugs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 
 use std::env;
+use std::time::{SystemTime, Duration};
+use std::sync::{Arc, Mutex};
 use std::ffi::OsString;
 use std::process::{Command, Stdio};
 
@@ -12,7 +14,7 @@ extern crate log;
 extern crate simplelog;
 use simplelog::{TermLogger, LevelFilter};
 
-use cargo_metadata::{Message, Artifact};
+use cargo_metadata::{Message};
 
 extern crate cargo_toml2;
 use cargo_toml2::{CargoToml, from_path};
@@ -34,6 +36,14 @@ struct Options {
     /// Command file to be passed to debugger
     command_file: Option<String>,
 
+    #[structopt(long = "filter")]
+    /// Filter to match against multiple output files
+    filter: Option<String>,
+
+    #[structopt(long = "no-run")]
+    /// Print the debug command to the terminal and exit without running
+    no_run: bool,
+
     #[structopt(long = "log-level", default_value = "info")]
     /// Enable verbose logging
     level: LevelFilter,
@@ -44,6 +54,8 @@ fn main() {
     // Fetch args as an array for splitting
     let args: Vec<OsString> = env::args_os().map(|a| a ).collect();
 
+    println!("args: {:?}", args);
+
     // Split options by "--" as debugger configuration or passthrough to cargo
     let mut s = args.splitn(3, |v| v == "--");
     let mut config_opts = match s.next() {
@@ -52,14 +64,20 @@ fn main() {
     };
 
     // Filter out the first arg when run as a cargo subcommand
-    if let Some(o) = config_opts.get(1).clone() {
+    if let Some(o) = &config_opts.get(1).clone() {
         if o.to_str().unwrap() == "debug" {
-            config_opts = config_opts.drain(2..).collect();
+            config_opts.remove(1);
         }
     }
 
-    let cargo_opts = s.next();
-    let child_opts = s.next();
+    let cargo_opts: Option<Vec<_>> = match s.next() {
+        Some(o) => Some(o.iter().map(|v| v.to_str().unwrap().to_string() ).collect()),
+        None => None,
+    };
+    let child_opts: Option<Vec<_>> = match s.next() {
+        Some(o) => Some(o.iter().map(|v| v.to_str().unwrap().to_string() ).collect()),
+        None => None,
+    };
 
     // Load options
     let o = Options::from_iter(&config_opts);
@@ -119,37 +137,115 @@ fn main() {
     handle.wait().expect("cargo command failed, try running the command directly");
     trace!("command executed");
 
-    // Find the output we care about
-    let outputs: Vec<&Artifact> = artifacts.iter().filter(|a| a.target.name == package ).collect();
+    // Find the output(s) we care about
+    let outputs: Vec<_> = artifacts.iter().filter_map(|a| {      
+        if let Some(x) = &a.executable {
+            return Some(x.clone())
+        }
+
+        None
+    } ).collect();
     trace!("found {} outputs: {:?}", outputs.len(), outputs);
 
-    let bin = outputs.get(0).map(|o| o.executable.clone() ).expect("no output artifacts found").expect("output artifact does not contain binary file");
+    // Filter / select outputs
+    let bin = match o.filter {
+        Some(f) => {
+            outputs.iter().find(|p| {
+                let file_name = p.file_name().unwrap().to_str().unwrap();
+                file_name.starts_with(&f)
+            } ).expect("no fi")
+        },
+        None => {
+            if outputs.len() > 1 {
+                error!("found multiple output arguments, pass --filter=X argument to select a specific output");
+                let names: Vec<_> = outputs.iter().map(|o| o.file_name().unwrap() ).collect();
+                error!("{:#?}", names);
+                return
+            }
 
-    info!("binary: {:?}", bin);
+            outputs.get(0).expect("no viable output artifacts found")
+        }
+    };
 
-    let mut debug_cmd = Command::new(o.debugger);
+    info!("selected binary: {:?}", bin);
 
-    if let Some(_opts) = child_opts {
-        // Forward child arguments if provided
-        debug_cmd.arg("--args");
+    let debugger = o.debugger;
+
+    let mut debug_args: Vec<String> = vec![];
+
+    if debugger.ends_with("gdb") {
+        // Prepare GDB to accept child options
+        if let Some(_opts) = &child_opts {
+            debug_args.push("--args".to_string());
+        }
+
+        // Append command file if provided
+        if let Some(command_file) = o.command_file {
+            debug_args.push("--command".to_string());
+            debug_args.push(command_file);
+        }
+
+        // Specify file to be debugged
+        debug_args.push(bin.clone().to_str().unwrap().to_string());
+
+        // Append child options
+        if let Some(opts) = &child_opts {
+            debug_args.append(&mut opts.clone());
+        }
+    } else if debugger.ends_with("lldb") {
+        // Specify file to be debugged
+        debug_args.push("--file".to_string());
+        debug_args.push(bin.clone().to_str().unwrap().to_string());
+
+        // Append command file if provided
+        if let Some(command_file) = o.command_file {
+            debug_args.push("--source".to_string());
+            debug_args.push(command_file);
+        }
+
+        // Append child options
+        if let Some(opts) = child_opts {
+            debug_args.push("--".to_string());
+            debug_args.append(&mut opts.clone());
+        }
+    } else {
+        error!("unsupported or unrecognised debugger {}", debugger);
+        return;
     }
 
-    if let Some(command_file) = o.command_file {
-        debug_cmd.arg("--command");
-        debug_cmd.arg(command_file);
+    trace!("synthesized debug arguments: {:?}", debug_args);
+
+    if o.no_run {
+        trace!("no-run selected, exiting");
+        println!("Debug command: ");
+        println!("{} {}", &debugger, debug_args.join(" "));
+        std::process::exit(0);
     }
 
-    debug_cmd.arg(bin.into_os_string());
+    let b = Arc::new(Mutex::new(SystemTime::now()));
 
-    if let Some(opts) = child_opts {
-        debug_cmd.args(opts);
-    }
+    // Override ctrl+c handler to avoid premature exit
+    // TODO: this... doesn't stop the rust process exiting..?
+    ctrlc::set_handler(move || {
+        warn!("CTRL+C");
+        let mut then = b.lock().unwrap();
+        let now = SystemTime::now();
+        if now.duration_since(*then).unwrap() > Duration::from_secs(1) {
+            std::process::exit(0);
+        } else {
+            *then = now;
+        }
+    }).expect("Error setting Ctrl-C handler");
+
+
+    let mut debug_cmd = Command::new(&debugger);
+    debug_cmd.args(debug_args);
 
     trace!("synthesized debug command: {:?}", debug_cmd);
-
+    
     debug_cmd.status().expect("error running debug command");
 
-
+    trace!("debug command done");
 }
 
 
